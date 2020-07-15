62b9965fd5f104ba5e7dff0aa55df48e09c4d8bf
Shinanubis <eric_maginot@hotmail.fr>
2020-07-15 22:09:18 +0200

    add login/logout function

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'article.apps.ArticleConfig',
     'comment.apps.CommentConfig',
     'django.contrib.admin',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
