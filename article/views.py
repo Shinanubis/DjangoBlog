bad4d07365545021b47ac30de0eee0519d2751b3
Shinanubis <eric_maginot@hotmail.fr>
2020-07-15 09:16:05 +0200

    minor corrections

--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render
 from .models import Article
 from comment.models import Comment, AddComment
 
@@ -11,12 +11,12 @@ def index(req):
 
 def detail(req, article_id):
     article = get_object_or_404(Article, pk=article_id)
-    form = AddComment
+    form = AddComment()
     if req.method == 'POST':
-        form = AddComment(req.POST)
+        form = AddComment(req.POST or None)
         if form.is_valid():
             form.save()
-            redirect('/details.html')
+            form = AddComment()
     comments_list = Comment.objects.order_by('-created_at')
     context = {'article': article, 'comments_list': comments_list, 'form': form}
     return render(req, 'article/details.html', context)
--- comment/views.py
+++ comment/views.py
@@ -1,15 +1,2 @@
 from django.shortcuts import render
-from comment.models import AddComment
 
-
-def addcomment(req):
-    form = AddComment(req.POST or None)
-    if form.is_valid():
-        author = form.cleaned_data['author']
-        content = form.cleaned_data['content']
-        related_paper = form.cleaned_data['related_paper']
-        created_at = form.cleaned_data['created_at']
-
-        envoi = True
-
-    return render(req, 'article/details.html', locals())
